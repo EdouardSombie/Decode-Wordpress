0fe9381c7d68873466cce10ee129469ff40d2623
EdouardSombie <edouard@voltajazz.com>
2025-10-24 15:53:37 +0200

    avec le css...

--- WP-decode-2025/wp-content/themes/Decode-theme/template-parts/post-list.php
+++ WP-decode-2025/wp-content/themes/Decode-theme/template-parts/post-list.php
@@ -1,14 +1,15 @@
-<?php 
+<?php
 $args = [
     'post_type' => 'post'
 ];
 $posts = get_posts($args);
 ?>
 <ul class="post-list">
-<?php foreach($posts as $p){ ?>
-    <li>
-        <a href="<?= get_permalink($p) ?>"><?= $p->post_title ?></a>
-        <time><?= wp_date('j F Y', strtotime($p->post_date) )  ?></time>
-    </li>
-<?php } ?>
+    <?php foreach ($posts as $p) { ?>
+        <li>
+            <a href="<?= get_permalink($p) ?>"><?= $p->post_title ?>
+                <time><?= wp_date('j F Y', strtotime($p->post_date))  ?></time>
+            </a>
+        </li>
+    <?php } ?>
 </ul>
